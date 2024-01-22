d38576018035d6c3a63041c3a4eb518d3224227b
MAZIANE wassim <mazianewassim@gmail.com>
2024-01-22 16:29:07 +0100

    Beginner's Quest 0000 challenge 0

---
+++ beginnersQuest/0000/challenge0/README.md
@@ -0,0 +1,2 @@
+we are presented with a ceasar cipher, knowing that the flag format is FLAG{...}, and knowing that the string SYNT{ebgngr_gung_nycunorg} is obviously the flag, 
+we can deduce that the offset used in this ceasar cipher.
---
+++ beginnersQuest/0000/challenge0/decryption.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import string
+enc = "Naljnl, Pnrfne jnf n fxvyyrq pbzzhavpngbe, naq ur hfrq n inevrgl bs zrgubqf gb xrrc uvf zrffntrf frperg sebz uvf rarzvrf. Bar bs gurfr zrgubqf jnf gur Pnrfne pvcure, n fvzcyr grpuavdhr gb boshfpngr pbzzhavpngvbaf. SYNT{ebgngr_gung_nycunorg}"
+dec = ""
+alphabet = list(string.ascii_lowercase)
+offset = alphabet.index('s') - alphabet.index('f')
+for character in enc:
+    if character.lower() in alphabet:
+        charIndex = alphabet.index(character.lower())
+        dec += alphabet[(charIndex + offset) % 26] 
+    else:
+        dec += character
+print(dec)
